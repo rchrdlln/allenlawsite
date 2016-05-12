65498628cd9b58400a6c819997ef17d02f2d337e
rchrdlln <rchrdlln@gmail.com>
2016-05-12 19:52:28 -0400

    added change of address notice

--- rahome.php
+++ rahome.php
@@ -1,3 +1,13 @@
+
+<p class="content" margin-left="15%" margin-right="15%">
+<strong>New Address - Stay Tuned!</strong>
+</p>
+<p class="contentleft" margin-left="15%" margin-right="15%">
+Allen Law is relocating to Raleigh, North Carolina in June 2016. After May 15, 2016 <b>please contact me via telephone</b> prior to sending mail to the Carrboro address above.
+</p>
+<p class="contentleft" margin-left="15%" margin-right="15%">
+I already have quite a few clients located in Wake County and look forward to serving more Wake County residents. I will continue to represent Durham, Orange and Chatham County clients in state and federal court, as well as clients across North Carolina. 
+</p>
 <p class="content" margin-left="15%" margin-right="15%">
 A North Carolina business and employment law attorney proudly serving individuals and small business clients located in Durham County, Orange County, Wake County, Chatham County, Alamance County, and elsewhere. 
 </p>
