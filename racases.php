8acda789c3a92e314b8b27533eedd2851087d7ab
rchrdlln <rchrdlln@gmail.com>
2016-05-12 20:45:53 -0400

    updated cases

--- racases.php
+++ racases.php
@@ -1,85 +1,116 @@
 <div class="content">
 
-  <!--
-    <p>My clients are individuals and small businesses. They include: </p>
-<ul>
-<li>tech startups
-</li><li>social entrepreneurs / triple bottom line businesses
-</li><li>medical professionals
-</li><li>nonprofits
-</li><li>retail businesses
-</li><li>consultants
-</li><li>executives
-</li><li>homeowners
-</li><li>workers experiencing employment disputes
-</li></ul>
--->
-
-<!--
-<p>Some representative small business clients:</p>
-<table border="0" width="200">
-<tr>
-<td><a href="/web/20140715050735/http://www.littlegreensoftware.com/" target="_new">Little Green Software</a></td>
-<td><a href="/web/20140715050735/http://www.zuzii.com/" target="_new">Zuzii</a></td>
-</tr>
-<tr>
-<td><a href="/web/20140715050735/http://www.rubberbanditz.com/" target="_new">Rubberbanditz</a></td>
-<td><a href="/web/20140715050735/http://www.changeeducate.org/" target="_new">ChangeEducate</a></td>
-</tr>
-</table>
--->
 <div align="left">
 <p>Some examples of work I've done:</p>
-<p><strong>Small Business Law</strong></p>
+<p><strong>Transactional: Science/Technology/Health/Wellness</strong></p>
 <ul class="content">
-<li>Represented a biotech contractor in suit for unpaid commissions filed in Durham County, NC court, resulting in significant settlement.
-</li><li>Assisted a mobile applications developer in amending and restating its operating agreement, preparing equity agreements for new employees, and preparing employment agreements and non-disclosure agreements.
-</li><li>Drafted data licensing agreement for international personal services company.
-</li><li>Drafted and reviewed sales, licensing and employment agreements for a fitness equipment company.
-</li><li>Successfully defended employer in claim for money owed in Wake County, NC court, obtaining dismissal of all claims.
-</li><li>Created business entities (LLCs and S-corps) for a variety of small businesses.
-</li><li>Created one of the first benefit corporations in the state of Virginia (July 2011).
-</li></ul>
-<p><strong>Employment Law</strong></p>
+	<li>Drafted and reviewed employment, services, noncompete, nondisclosure, and severance agreements for local startups, key employees, and contractors.
+	</li> 
+	<li>Assisted a mobile applications developer in amending and restating its operating agreement, preparing equity agreements for new employees, and preparing employment agreements and non-disclosure agreements.
+	</li>
+	<li>Drafted data licensing agreement for international personal services company.
+	</li>
+	<li>Drafted and reviewed sales, licensing and employment agreements for a fitness equipment company.
+	</li>
+	<li>Created business entities (LLCs and S-corporations) for a variety of small businesses.
+	</li>
+	<li>Successfully stopped unauthorized marketing practices by an Internet competitor of an fitness equipment company.
+	</li>
+</ul>
+
+<p><strong>Transactional: Food/Beverage/Live Entertainment</strong></p>
 <ul class="content">
-<li>Successfully represented Alamance County, NC employee with regard to gender discrimination, pregnancy discrimination, and retaliation claims, reaching a confidential settlement.
-</li><li>Successfully represented Pitt County, NC employee with regard to racial discrimination and retaliation claims, reaching a confidential settlement.
-</li><li>Represented probationary teacher in employment negotiations with Wake County, NC school board.
-</li><li>Represented employees in wage and hour suit against Wake County, NC employer.
-</li><li>Represented executive of a New Hanover County, NC nonprofit organization in dispute with board of directors.
-</li><li>Advised biomedical industry professional regarding severance agreement negotiations.
-</li><li>Won unemployment benefits for a wide variety of terminated employees in appeals hearings before the North Carolina Employment Security Commission. For more details on my unemployment appeals practice, visit my <a href="/web/20140715050735/http://allenlawnc.com/?q=node/8" target="_new">North Carolina Unemployment Appeals Lawyer</a> page.
-</li></ul>
-<p><strong>Entertainment Law</strong></p>
+	<li>Advised clients on and drafted agreements for a variety of transactions including:
+		<br>
+		<br>
+		<ul>
+		<li>Sale of an entrepreneur's interests in two LLCs in the wine and spirits industry
+		</li>
+		<li>Purchase of the assets of a nightclub, including intellectual property
+		</li>
+		<li>Transfer of LLC interests and employment and equity agreements for a restaurant franchise owner
+		</li>
+		<li>Purchase of an LLC member's interest in a local brewing company
+		</li>
+		</ul>
+	</li>
+	</ul>
+	
+<p><strong>Litigation: Employment/Civil Rights/Health Fields</strong></p>
 <ul class="content">
-<li>Represented nationally known hip-hop music group in negotiation of a recording contract with an independent label and production agreements with outside producers.
-</li><li>Represented filmmakers in a business dispute regarding the terms of a North Carolina limited liability company ("LLC") operating agreement.
-</li><li>Filed trademark application for singer and entertainer.
-</li></ul>
-<p><strong>Intellectual Property: Copyright/Trademark/Domain Name</strong></p>
+	<li>Lead counsel in civil rights, wrongful death, and medical malpractice suit against Eastern North Carolina sheriff and jail medical services providers, resulting in significant settlements.
+	</li>
+	<li>Obtained jury verdict for four health care industry clients on all claims in unpaid wage and overtime suit against former employer in Durham County Superior Court.
+	</li>
+	<li>Obtained significant settlement at mediation for Eastern North Carolina health care industry client for claims including wrongful termination, disability discrimination and Family and Medical Leave Act ("FMLA") violations.
+	</li>
+	<li>Obtained settlement at mediation for health care industry client in suit filed in Chatham County for wrongful discharge, overtime violations, and violations of the Retaliatory Employment Discrimination Act ("NC REDA").
+	</li>
+	<li>Obtained settlement at EEOC conciliation for Eastern North Carolina client regarding allegations of Title VII racial discrimination and retalation. 
+	</li>
+	<li>Successfully represented Alamance County employee with regard to gender discrimination, pregnancy discrimination, and retaliation claims, reaching a confidential settlement.
+	</li>
+	<li>Successfully represented Pitt County employee with regard to racial discrimination and retaliation claims, reaching a confidential settlement.
+	</li>
+	<li>Obtained award in Wake County court for retaliation and wrongful termination of 3 Raleigh health care industry employees.
+	</li>
+</ul>
+
+<p><strong>Litigation: Business</strong></p>
 <ul class="content">
-<li>Successfully defended a web developer against claims of copyright and trademark infringement, resulting in dismissal of claims.
-</li><li>Successfully represented a defendant in a BitTorrent copyright infringement claim filed by an overseas adult entertainment company. After defendant's motions to sever and to proceed anonymously were granted, plaintiff accepted significantly reduced settlement.
-</li><li>Advised a health and wellness entrepreneur on domestic and international trademark issues.
-</li><li>Advised on trademark issues and filed trademark applications for a niche clothing business with a significant Web presence.
-</li><li>Advised on trademark issues and filed federal trademark registration applications for a Charlotte, NC marketing company.
-</li><li>Drafted cease and desist letter and successfully stopped unauthorized marketing practices by an Internet competitor of an fitness equipment company.
-</li><li>Assisted an SEO entrepreneur in resolving Uniform Domain Name Resolution Procedure initiated by a giant of the tech industry.
-</li><li>Advised a medical professional regarding trademark and domain name issues.
-</li><li>Prepared and filed federal trademark registrations for a variety of businesses, including a Charlotte, NC marketing company and a Raleigh, NC data hosting company.
-</li></ul>
-<p><strong>Consumer Law</strong></p>
+	<li>Obtained jury verdict for e-commerce consultant in Wake County court against claims including fraud, breach of contract and trademark infringement and winning judgment for client on counterclaims of unfair trade practices.
+	</li>
+	<li>Obtained significant settlement for biotech contractor in suit for unpaid commissions in Durham County court. 
+	</li>
+	<li>Successfully defended employer in claim for money owed in Wake County court, obtaining dismissal of all claims.
+	</li>
+</ul>
+
+<p><strong>Transactional: Entertainment</strong></p>
 <ul class="content">
-<li>Successfully represented purchaser of new car in NC Lemon Law dispute with manufacturer, with manufacturer agreeing to repurchase car and pay attorneys' fees prior to arbitration hearing.
-</li><li>Negotiated a settlement regarding property title issues between a homebuyer and a developer, avoiding litigation and foreclosure proceedings.
-</li><li>Advised a North Carolina homeowner regarding options in dispute with a home mortgage lender, including short sale, deed in lieu, and foreclosure prevention programs.
-</li></ul>
-<p><strong>Criminal Law</strong></p>
+	<li>Represented nationally known hip-hop music group in negotiation of a recording contract with an independent label and production agreements with outside producers.
+	</li>
+	<li>Represented filmmakers in a business dispute regarding the terms of a North Carolina limited liability company ("LLC") operating agreement.
+	</li>
+</ul>
+
+<p><strong>Transactional and Litigation: Intellectual Property: Copyright/Trademark/Domain Name</strong></p>
+	<ul class="content">
+	<li>Successfully defended a web developer against claims of copyright and trademark infringement, resulting in dismissal of claims.
+	</li>
+	<li>Defended client in trademark cancellation action before federal Trademark Trial and Appeal Board ("TTAB"), resulting in amicable settlement.
+	</li>
+	<li>Successfully represented a defendant in a BitTorrent copyright infringement claim filed by an overseas adult entertainment company. After defendant's motions to sever and to proceed anonymously were granted, plaintiff accepted significantly reduced settlement.
+	</li>
+	<li>Advised a health and wellness entrepreneur on domestic and international trademark issues.
+	</li>
+	<li>Advised on trademark issues and filed trademark registration applications for a variety of businesses, including: 
+		<br>
+		<br>
+		<ul>
+		<li>a national children's clothing business with a significant Web presence
+		</li>
+		<li>a Raleigh, NC data hosting company
+		</li>
+		<li>a sports apparel startup
+		</li>
+		<li>a Charlotte marketing company
+		</li>
+		</ul>
+	</li>
+	<li>Assisted an SEO entrepreneur in resolving Uniform Domain Name Resolution Procedure initiated by a giant of the tech industry.
+	</li>
+</ul>
+
+<p><strong>Litigation: Consumer Law</strong></p>
 <ul class="content">
-<li>Successfully represented defendant on charges of assault by pointing a gun, obtaining acquittal at trial.
-</li><li>Successfully represented client in Chapter 50C order of protection hearing.
-</li><li>Defended individuals facing a variety of misdemeanor charges, including driving while impaired, assault with a deadly weapon, marijuana possession, possession of drug paraphernalia, and embezzlement.
-</li></ul>
+	<li>Successfully represented purchaser of new car in NC Lemon Law dispute with manufacturer, with manufacturer agreeing to repurchase car and pay attorneys' fees prior to arbitration hearing.
+	</li>
+	<li>Negotiated a settlement regarding property title issues between a homebuyer and a developer, avoiding litigation and foreclosure proceedings.
+	</li>
+	<li>Advised a North Carolina homeowner regarding options in dispute with a home mortgage lender, including short sale, deed in lieu, and foreclosure prevention programs.
+	</li>
+</ul>
 </div>
 
   </div>
